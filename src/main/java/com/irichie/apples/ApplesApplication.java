0444a3687d26dca8a6ae59958bb5338174bc23de
Imoh Obot <obotimoh20@gmail.com>
2022-02-08 17:59:39 +0100

    app init

---
+++ src/main/java/com/irichie/apples/ApplesApplication.java
@@ -0,0 +1,13 @@
+package com.irichie.apples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irichie/apples/ApplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irichie.apples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
